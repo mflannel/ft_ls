b3e10c4e9444144caddd5010fde70707ce83123f
mflannel <nikita.m.komarov@gmail.com>
2019-11-21 17:38:29 +0300

    check_flags

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "ls.h"
+#include <stdio.h>
 
 //-l, -R, -a, -r and -t.
 
@@ -9,7 +10,8 @@ int main(int ac, char **av)
     if(!check_flags(av, command))
      start_usage();
     else
-        execute_command(av, command);
+        printf("command executed\n");
+        //execute_command(av, command);
 }
 
 void start_usage()
@@ -23,26 +25,35 @@ int check_flags(char **av, t_options command)
 
     i = 0;
 
-    if(av[i] != '-')
+    if((char)av[1][0] != '-')
         return (0);
     i++;
 
-    while (av[i] != '\0')
+    while (av[1][i] != '\0')
     {
-        if (av[i] != (int)'l' || av[i] != 'R' || av[i] != 'a' || av[i] != 'r' | av[i] != 't')
+        if ((char)av[1][i] != 'l' && (char)av[1][i] != 'R' &&
+        (char)av[1][i] != 'a' &&
+        (char)av[1][i] != 'r' &&
+        (char)av[1][i] != 't')
             return (0);
-        if(av[i] == 'l')
-            command.l = 1;
-        if(av[i] == 'R')
-            command.R = 1;
-        if(av[i] == 'a')
-            command.a = 1;
-        if(av[i] == 'r')
-            command.r = 1;
-        if(av[i] == 't')
-            command.t = 1;
+
+            if((char)av[1][i] == 'l')
+                command.l = 1;
+            if((char)av[1][i] == 'R')
+                command.R = 1;
+            if((char)av[1][i] == 'a')
+                command.a = 1;
+            if((char)av[1][i] == 'r')
+                command.r = 1;
+            if((char)av[1][i] == 't')
+                command.t = 1;
         i++;
     }
+    printf("%d\n", command.l);
+    printf("%d\n", command.R);
+    printf("%d\n", command.a);
+    printf("%d\n", command.r);
+    printf("%d\n", command.t);
 }
 
 
